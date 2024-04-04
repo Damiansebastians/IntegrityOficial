19c31796f5de1322ce379e29b38b47bcd094fa2d
Damian Silvera <damiansebastians@gmail.com>
2024-04-04 23:44:38 +0200

    footer

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,14 +24,21 @@ export const Hero = () => {
         </main>
 
         <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
-          Build your React landing page effortlessly with the required sections
-          to your project.
+          !Artista tendencia en España!
         </p>
 
         <div className="space-y-4 md:space-y-0 md:space-x-4">
-          <Button className="w-full md:w-1/3">ADELANTO</Button>
+          <a href="https://open.spotify.com"></a>
+          <Button 
+          className="w-full md:w-1/3" 
+          style={{ 
+            backgroundColor: 'green',
+             color: 'white'
+            }}
+               
+             >SPOTIFY</Button>
 
-          <a
+          <a style={{ backgroundColor: 'red', color: 'white' }}
             href="https://youtube.com"
             target="_blank"
             className={`w-full md:w-1/3 ${buttonVariants({
